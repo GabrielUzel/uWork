8af2e49e7ce790b609e67b42f68812ba790b2122
GabrielUzel <gabriel.uzel1@gmail.com>
2022-10-27 22:23:09 -0300

    Renomeando pastas

--- src/controller/ControllerPessoaFisica.java
+++ src/controller/ControllerPessoaFisica.java
@@ -1,5 +1,40 @@
 package controller;
 
+import java.sql.SQLException;
+import java.util.ArrayList;
+import DAO.PessoaFisicaDAO;
+import model.PessoaFisica;
+
 public class ControllerPessoaFisica {
-    
+    public ControllerPessoaFisica() {}
+
+    private PessoaFisicaDAO pessoaFisicaDao = new PessoaFisicaDAO();
+
+    public void cadastraFormacao(String cpf, String categoriaServico) throws ClassNotFoundException, SQLException {
+        PessoaFisica pessoaFisica = new PessoaFisica();
+        pessoaFisica.setCPF(cpf);
+        pessoaFisica.setCategoriaServico(categoriaServico);
+        pessoaFisicaDao.criarPessoaFisica(pessoaFisica);
+    }
+
+    public ArrayList<PessoaFisica> listarTodasPessoasFisicas() throws ClassNotFoundException, SQLException {
+        ArrayList<PessoaFisica> pessoasFisicasList = (ArrayList<PessoaFisica>) pessoaFisicaDao.listarTodasPessoasFisicas();
+        return pessoasFisicasList;
+    }
+
+    public PessoaFisica pesquisarPessoaFisica(int id) throws ClassNotFoundException, SQLException {
+        PessoaFisica pessoaFisica = pessoaFisicaDao.pesquisarPessoaFisica(id);
+        return pessoaFisica;
+    }
+
+    public void atualizarPessoaFisica(String cpf, String categoriaServico) throws ClassNotFoundException, SQLException {
+        PessoaFisica pessoaFisica = new PessoaFisica();
+        pessoaFisica.setCPF(cpf);
+        pessoaFisica.setCategoriaServico(categoriaServico);
+        pessoaFisicaDao.atualizarPessoaFisica(pessoaFisica);
+    }
+
+    public void deletarPessoaFisica(PessoaFisica pessoaFisica) throws ClassNotFoundException, SQLException {
+        pessoaFisicaDao.deletarPessoaFisica(pessoaFisica);
+    }
 }
--- src/controller_telas/ControllerCadastroVaga.java
+++ src/controller_telas/ControllerCadastroVaga.java
@@ -1,4 +1,4 @@
-package ControllerTelas;
+package controller_telas;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
