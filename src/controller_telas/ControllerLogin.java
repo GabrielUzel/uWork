fcadbf5f2cd59c24172495e26e78757a50f075ea
wesleiferreira98 <57416280+wesleiferreira98@users.noreply.github.com>
2022-10-29 22:43:29 -0300

    Update ControllerLogin.java

--- src/controller_telas/ControllerLogin.java
+++ src/controller_telas/ControllerLogin.java
@@ -1,10 +1,16 @@
 package controller_telas;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import java.io.IOException;
+import Controller.ChamaOutraTela;
+import Controller.ClassAlerta;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
 public class ControllerLogin{
+    private ChamaOutraTela tela = new ChamaOutraTela();
+    private ClassAlerta alerta = new ClassAlerta();
     @FXML
     private Button btn_entrar;
 
@@ -19,4 +25,25 @@ public class ControllerLogin{
 
     @FXML
     private TextField txt_senha;
+    
+    @FXML
+    void btn_cadastrarFUN(ActionEvent event) {
+        try {
+            tela.chamar("/View/CadastroUsuario.fxml","Cadastro de Usuario");
+        } catch (IOException e) {
+            e.printStackTrace();
+            alerta.erro("Erro", e.getMessage(), "aconteceu um erro");
+
+        }
+    }
+
+    @FXML
+    void btn_entrarFUN(ActionEvent event) {
+
+    }
+
+    @FXML
+    void txt_esquiFUN(ActionEvent event) {
+
+    }
 }
