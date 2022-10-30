afbdc21b8225fd95fe782f1bf3ae0ba43a1e4a61
wesleiferreira98 <57416280+wesleiferreira98@users.noreply.github.com>
2022-10-29 22:56:34 -0300

    Add files via upload

---
+++ src/controller/ChamaOutraTela.java
@@ -0,0 +1,22 @@
+package Controller;
+
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class ChamaOutraTela {
+    public ChamaOutraTela(){}
+
+    public void chamar(String caminhoTela,String titulo) throws IOException{
+        Stage stage =new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource(caminhoTela));
+            Scene scene = new Scene(root);
+            stage.setTitle(titulo);
+            stage.setScene(scene);
+            stage.show();
+            stage.resizableProperty().setValue(false);
+    }
+}
