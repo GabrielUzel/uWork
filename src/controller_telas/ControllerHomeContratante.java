cdf2396c02687de85a6df6a74a314e4be81c186e
Jose-Junio <josejuniorbdj@gmail.com>
2022-11-20 19:26:20 -0300

    Erro Home e HomeContratante arrumada

---
+++ src/controller_telas/ControllerHomeContratante.java
@@ -0,0 +1,33 @@
+package controller_telas;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class ControllerHomeContratante {
+
+    @FXML
+    void btAdicionaVaga(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btListaVagas(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btPerfil(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btRemoveVaga(ActionEvent event) {
+
+    }
+
+    @FXML
+    void btn_voltarFUN(ActionEvent event) {
+
+    }
+}
