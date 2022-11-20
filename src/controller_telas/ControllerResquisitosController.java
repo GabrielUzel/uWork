31419b4bb5f89689a136d4024939647b0b6984c6
wesleiferreira98 <57416280+wesleiferreira98@users.noreply.github.com>
2022-11-19 23:19:59 -0300

    ajustes de imports

--- src/controller_telas/ControllerResquisitosController.java
+++ src/controller_telas/ControllerResquisitosController.java
@@ -11,7 +11,6 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 
