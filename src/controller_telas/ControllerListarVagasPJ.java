8f8d386649fdbe57cc8700590306dbab15afb914
Jose-Junio <josejuniorbdj@gmail.com>
2022-11-22 22:06:40 -0300

    a

--- src/controller_telas/ControllerListarVagasPJ.java
+++ src/controller_telas/ControllerListarVagasPJ.java
@@ -1,18 +1,11 @@
 package controller_telas;
 
 import java.net.URL;
-import java.sql.Date;
 import java.sql.SQLException;
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Observable;
 import java.util.ResourceBundle;
 
-import DAO.CandidaturaDAO;
-import DAO.PessoaJuridicaDAO;
 import DAO.VagaDAO;
-import controller.ChamaOutraTela;
-import controller.ClassAlerta;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -21,8 +14,6 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
 import model.CacheUsuario;
 import model.Vaga;
