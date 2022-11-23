fc55450c8ab4e078d11b5dbf7a834f05a234f4e6
GabrielUzel <gabriel.uzel1@gmail.com>
2022-11-22 23:06:38 -0300

    Update ControlerCadastroUsuarios

--- src/controller_telas/ControllerCadastroUsuarios.java
+++ src/controller_telas/ControllerCadastroUsuarios.java
@@ -1,7 +1,6 @@
 package controller_telas;
 
 import java.sql.SQLException;
-
 import DAO.UsuarioDAO;
 import controller.ClassAlerta;
 import javafx.event.ActionEvent;
@@ -11,7 +10,6 @@ import javafx.scene.control.TextField;
 import model.Usuario;
 
 public class ControllerCadastroUsuarios {
-
     @FXML
     private Button btn_voltar;
 
@@ -60,31 +58,32 @@ public class ControllerCadastroUsuarios {
         String Email = tfEmail.getText();
         String senha = tfSenha.getText();
         Long telefone = (long) 0;
-        if(!tfTelefone.getText().isEmpty()){
+        if(!tfTelefone.getText().isEmpty()) {
             telefone = Long.parseLong(tfTelefone.getText());
-        }        
+        }
+
         String pais = tfPais.getText();
         String estado = tfEstado.getText();
         Long cep = (long) 0;
-        if(!tfCep.getText().isEmpty()){
+        if(!tfCep.getText().isEmpty()) {
             cep = Long.parseLong(tfCep.getText());
-        }   
+        }
+
         String cidade = tfCidade.getText();
         String bairro = tfBairro.getText();
         String logradouro = tfLogradouro.getText();
         String complemento = tfComplemento.getText();
         int numero = 0;
-        if(!tfNumero.getText().isEmpty()){
+        if(!tfNumero.getText().isEmpty()) {
             numero = Integer.parseInt(tfNumero.getText());
         }
 
-        Usuario u = new Usuario(nome, Email, senha, telefone, pais, estado, cep, cidade, bairro, logradouro, complemento, numero);
-        
-        UsuarioDAO ud = new UsuarioDAO();
+        Usuario usuario = new Usuario(nome, Email, senha, telefone, pais, estado, cep, cidade, bairro, logradouro, complemento, numero);
+        UsuarioDAO usuarioDao = new UsuarioDAO();
 
         ClassAlerta alerta = new ClassAlerta();
         try {
-            ud.criarUsuario(u);
+            usuarioDao.criarUsuario(usuario);
             alerta.confirmacao("Concluido", "O usuário foi cadastrado com sucesso", "");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
